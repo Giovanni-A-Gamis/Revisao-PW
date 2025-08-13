acc93907ce2d15b7eea19d85f8f2bd54bdd48409
GIOVANNI ALCARAZ GAMIS <giovanni.gamis@etec.sp.gov.br>
2025-08-13 08:35:48 -0300

    quase completo

---
+++ revisaopw/src/App.jsx
@@ -0,0 +1,23 @@
+//Bibliotecas
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './screens/Home/index';
+import Contador from './screens/Contador/index';
+import Usuarios from './screens/Usuarios/index';
+
+//Função principal
+function App() {
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to='/'>Home</Link> | <Link to='/contador'>Contador</Link> | <Link to='/usuarios'>Usuarios</Link>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Home />}/>
+                <Route path='/contador' element={<Contador />}/>
+                <Route path='/usuarios' element={<Usuarios />}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
---
+++ revisaopw/src/screens/Home/index.jsx
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Boas-Vindas!!!</h1>
+}
+
+export default Home;
